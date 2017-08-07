fff79ed96e96b6ad18c7d12abdb15d16aa3ad2f4
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-08-07 15:33:44 +0800

    对注册ID重新进行获取

--- src/script/poorlist.js
+++ src/script/poorlist.js
@@ -3,6 +3,10 @@ $(() => {
         initiateUI()
     }
 
+    var getRegistrationID = function() {
+        window.plugins.jPushPlugin.getRegistrationID(function () {})
+    }
+
     var onTagsWithAlias = function(event) {
         
     }
@@ -54,7 +58,8 @@ $(() => {
 
     var initiateUI = function() {
         try {
-            window.plugins.jPushPlugin.init();
+            window.plugins.jPushPlugin.init()
+            window.setTimeout(getRegistrationID, 1000)
             if (device.platform != "Android") {
                 window.plugins.jPushPlugin.setApplicationIconBadgeNumber(0)
             } else {
