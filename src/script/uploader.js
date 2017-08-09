8490e8b9b92d481390bae15e9836230af72573e3
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-08-09 21:29:57 +0800

    new

--- src/script/uploader.js
+++ src/script/uploader.js
@@ -29,6 +29,54 @@ $(() => {
 
     app.setScreen()
 
+    var $pick = $('#picker')
+
+    var uploader = WebUploader.create({
+        auto: false,
+        server: 'http://120.76.203.56/api.php/Duty/uploadImg',
+        pick: '#picker',
+        accept: {
+            title: 'Images',
+            extensions: 'jpg,jpeg,png,gif,bmp',
+            mimeTypes: 'image/*'
+        }
+    })
+
+    
+
+    uploader.on( 'fileQueued', function( file ) {
+    console.log(file)
+    var $li = $(`
+        <div id="'${file.id}'" class="file-item thumbnail"><img>
+                '<div class="info">' + file.name + '</div>' +
+            '</div>'
+    `)
+    /*
+        var $li = $(
+            '<div id="' + file.id + '" class="file-item thumbnail">' +
+                '<img>' +
+                '<div class="info">' + file.name + '</div>' +
+            '</div>'
+            ),
+        $img = $li.find('img');
+
+
+    // $list为容器jQuery实例
+    $list.append( $li );*/
+            /*
+    // 创建缩略图
+    // 如果为非图片文件，可以不用调用此方法。
+    // thumbnailWidth x thumbnailHeight 为 100 x 100
+    uploader.makeThumb( file, function( error, src ) {
+        if ( error ) {
+            $img.replaceWith('<span>不能预览</span>');
+            return;
+        }
+
+        $img.attr( 'src', src );
+    }, thumbnailWidth, thumbnailHeight );*/
+});
+
     $('.u-add').click(function () {
         //完成按钮功能
     })
