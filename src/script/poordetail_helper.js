8b1ba32ac7170f755f6a80ca027f4ea5c35c1a67
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-08-11 16:35:47 +0800

    new

--- src/script/poordetail_helper.js
+++ src/script/poordetail_helper.js
@@ -1,94 +1,99 @@
 $(() => {
-
-    //创建根节点对象
-    var app = {
-        el: $('#app'),
-        setScreen: () => {
-            app.el.css({"height": `${window.innerHeight - 20}px`})
-        },
-        showMsg: (msg) => {
-            $('.msg').html(msg).show(() => {
-                $('.msg').css({'opacity': '1'})
-                setTimeout(() => {
-                    $('.msg').css({'opacity': '0'})
-                }, 2000)
-                setTimeout(() => {
-                    $('.msg').hide()
-                }, 3000)
-            })
-        },
-        getUrlPrama: (name) => {
-            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
-            var r = window.location.search.substr(1).match(reg)
-            if (r != null) {
-                return unescape(r[2])
-            } 
-            return null
+    
+        //创建根节点对象
+        var app = {
+            el: $('#app'),
+            setScreen: () => {
+                app.el.css({"height": `${window.innerHeight - 20}px`})
+            },
+            showMsg: (msg) => {
+                $('.msg').html(msg).show(() => {
+                    $('.msg').css({'opacity': '1'})
+                    setTimeout(() => {
+                        $('.msg').css({'opacity': '0'})
+                    }, 2000)
+                    setTimeout(() => {
+                        $('.msg').hide()
+                    }, 3000)
+                })
+            },
+            getUrlPrama: (name) => {
+                var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
+                var r = window.location.search.substr(1).match(reg)
+                if (r != null) {
+                    return unescape(r[2])
+                } 
+                return null
+            }
         }
-    }
-
-    //调用方法
-    app.setScreen()
-    console.log(app.getUrlPrama('table_id'))
-
-    //生成列表链接
-    $('#condition').attr('href', `poordetail_condition.html?table_id=${app.getUrlPrama('table_id')}`)
-    $('#basic').attr('href', `poordetail_basic.html?table_id=${app.getUrlPrama('table_id')}`)
-    $('#result').attr('href', `poordetail_result.html?table_id=${app.getUrlPrama('table_id')}`)
-    $('#record').attr('href', `poordetail_record.html?table_id=${app.getUrlPrama('table_id')}`)
-    $('#punchlist').attr('href', `punchlist.html?table_id=${app.getUrlPrama('table_id')}`)
-
-
-    //侧边搜索框弹出
-    $('.m-dropdown .u-show').click(function () {
-        $('.u-hide').addClass('animOut')
-        $('.u-mask').show()
-    })
-
-    $('.u-mask').click(function () {
-        getBack()
-    })
-
-    //侧边栏回收函数
-    function getBack() {
-        $('.u-hide').removeClass('animOut')
-        $('.u-hide').addClass('animIn')
-        setTimeout(function () {
-            $('.u-hide').removeClass('animIn')
-        }, 350)
-        $('.u-mask').hide()
-    }
-
-    $('.arrow').click(function () {
-        if ($(this).hasClass('up')) {
-            $(this).removeClass('up').addClass('down')
-            $(this).parent().next().slideToggle()
-        } else {
-            $(this).removeClass('down').addClass('up')
-            $(this).parent().next().slideToggle()
+    
+        //调用方法
+        app.setScreen()
+        console.log(app.getUrlPrama('table_id'))
+    
+        //生成列表链接
+        $('#condition').attr('href', `poordetail_condition.html?table_id=${app.getUrlPrama('table_id')}`)
+        $('#basic').attr('href', `poordetail_basic.html?table_id=${app.getUrlPrama('table_id')}`)
+        $('#result').attr('href', `poordetail_result.html?table_id=${app.getUrlPrama('table_id')}`)
+        $('#record').attr('href', `poordetail_record.html?table_id=${app.getUrlPrama('table_id')}`)
+        $('#punchlist').attr('href', `punchlist.html?table_id=${app.getUrlPrama('table_id')}`)
+    
+    
+        //侧边搜索框弹出
+        $('.m-dropdown .u-show').click(function () {
+            $('.u-hide').addClass('animOut')
+            $('.u-mask').show()
+        })
+    
+        $('.u-mask').click(function () {
+            getBack()
+        })
+    
+        //侧边栏回收函数
+        function getBack() {
+            $('.u-hide').removeClass('animOut')
+            $('.u-hide').addClass('animIn')
+            setTimeout(function () {
+                $('.u-hide').removeClass('animIn')
+            }, 350)
+            $('.u-mask').hide()
         }
+    
+        $('.arrow').click(function () {
+            if ($(this).hasClass('up')) {
+                $(this).removeClass('up').addClass('down')
+                $(this).parent().next().slideToggle()
+            } else {
+                $(this).removeClass('down').addClass('up')
+                $(this).parent().next().slideToggle()
+            }
+        })
+    
+        $.ajax({
+            url: 'http://www.hiphoon.com/api.php/Macro/poorDetail',
+            type: 'POST',
+            data: `table_id=${app.getUrlPrama('table_id')}`,
+            success: (data) => {
+                console.log(JSON.parse(data).data)
+    
+                $('#name').html(JSON.parse(data).data.poor.name)
+                $('#area').html(JSON.parse(data).data.poor.townname + '&nbsp;' + JSON.parse(data).data.poor.villagename)
+                $('#dutyname').html(JSON.parse(data).data.dutys[0].name)
+                $('#sex').html(JSON.parse(data).data.dutys[0].sex)
+                $('#telephone').html(JSON.parse(data).data.dutys[0].contacnumber)
+                $('#company').html(JSON.parse(data).data.dutys[0].orgname)
+                
+                if (JSON.parse(data).data.dutys[0].icon) {
+                    $('.photo .unit').append(`
+                        <img src='http://www.hiphoon.com${JSON.parse(data).data.dutys[0].icon}'>
+                    `)
+                }
+    
+                if (JSON.parse(data).data.dutys[0].photo) {
+                    $('.photo .unit').append(`
+                        <img src='http://www.hiphoon.com${JSON.parse(data).data.dutys[0].photo}'>
+                    `)
+                }
+            }
+        })
     })
-
-    $.ajax({
-        url: 'http://www.hiphoon.com/api.php/Macro/poorDetail',
-        type: 'POST',
-        data: `table_id=${app.getUrlPrama('table_id')}`,
-        success: (data) => {
-            console.log(JSON.parse(data).data)
-
-            $('#name').html(JSON.parse(data).data.poor.name)
-            $('#area').html(JSON.parse(data).data.poor.townname + '&nbsp;' + JSON.parse(data).data.poor.villagename)
-            $('#dutyname').html(JSON.parse(data).data.dutys[0].name)
-            $('#sex').html(JSON.parse(data).data.dutys[0].sex)
-            $('#telephone').html(JSON.parse(data).data.dutys[0].contacnumber)
-            $('#company').html(JSON.parse(data).data.dutys[0].orgname)
-
-            $('.photo').append(`
-                <div class="unit flex">
-                    <img src='http://www.hiphoon.com${JSON.parse(data).data.dutys[0].icon}'>
-                    <img src='http://www.hiphoon.com${JSON.parse(data).data.dutys[0].photo}'>
-                </div>
-            `)
-        }
-    })
-})
