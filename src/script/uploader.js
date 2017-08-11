51f6de0c658cfed075f41d8f7393b39060d9fb51
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-08-11 10:41:17 +0800

    new

--- src/script/uploader.js
+++ src/script/uploader.js
@@ -44,49 +44,61 @@ $(() => {
         }
     })
 
-    
-
     uploader.on( 'fileQueued', function( file ) {
-    console.log(file)
-    var $li = $(`
-        <div id="${file.id}" class="file-item thumbnail pre"><img style="width: 100%; height: 100%;"><a href="javascript:;" class="uploader_del"></a>
-        </div>
-    `)
-    var $img = $li.find('img')
+        console.log(file)
+        var $li = $(`
+            <div id="${file.id}" name="${file.name}" class="file-item thumbnail pre"><img><a href="javascript:;" class="uploader_del"></a>
+            </div>
+        `)
+        var $img = $li.find('img')
 
-    // $list为容器jQuery实例
-    $pick.before($li)
-    //$list.append(  )
-    $('.imglist').find('.uploader_del').unbind('click').click(function() {
-        var fname = $(this).parent().attr('name')
+        // $list为容器jQuery实例
+        $pick.before($li)
+        //$list.append(  )
+        $('.imglist').find('.uploader_del').unbind('click').click(function() {
+            var fname = $(this).parent().attr('name'),
+                fid = $(this).parent().attr('id')
 
-		for(var i = 0; i < fileNameArray.length; i++) {
+		    for(var i = 0; i < fileNameArray.length; i++) {
             
-			if(fname == fileNameArray[i]) {
-				fileArray.splice(i, 1)
-				fileNameArray.splice(i, 1)
-			}
-		}
-		$('#filearray').val(fileArray);
-		$(this).parent().remove();
-		if($('.imglist').find('.file-item').length == 0) {
-			$('.tip_text').show()
-		}
-	})      
-    // 创建缩略图
-    // 如果为非图片文件，可以不用调用此方法。
-    // thumbnailWidth x thumbnailHeight 为 100 x 100
-    uploader.makeThumb( file, function( error, src ) {
-        if ( error ) {
-            $img.replaceWith('<span>不能预览</span>');
-            return;
-        }
+			    if(fname == fileNameArray[i]) {
+				    fileArray.splice(i, 1)
+				    fileNameArray.splice(i, 1)
+			    }
+		    }
+            $(this).parent().remove()
+            uploader.removeFile( fid )
+            app.showMsg('已删除')
+	    })      
+        // 创建缩略图
+        // 如果为非图片文件，可以不用调用此方法。
+        // thumbnailWidth x thumbnailHeight 为 100 x 100
+        uploader.makeThumb( file, function( error, src ) {
+            if ( error ) {
+                $img.replaceWith('<span>不能预览</span>')
+                return;
+            }
 
-        $img.attr( 'src', src );
-    });
-});
+            $img.attr( 'src', src )
+        })
 
-    $('.u-add').click(function () {
-        //完成按钮功能
+        $('.u-add').click(function () {
+            //完成按钮功能
+            uploader.upload(file)
+            console.log('ok')
+        })
     })
+
+    uploader.on( 'uploadSuccess', function( file,response ) {
+
+        console.log(file)
+        console.log(response)
+        /*
+        if($.inArray(response.img_name, fileArray) < 0) {
+    				
+		    fileNameArray.push(file.name);
+		    fileArray.push(response.img_name);
+	    }
+    			*/
+	})
 })
