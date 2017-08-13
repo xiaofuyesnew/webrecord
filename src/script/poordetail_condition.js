d25bbfef3ca17f1e254a63a6bfb4ff16225e41a8
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-08-13 20:54:17 +0800

    new

--- src/script/poordetail_condition.js
+++ src/script/poordetail_condition.js
@@ -1,5 +1,4 @@
 $(() => {
-
     //创建根节点对象
     var app = {
         el: $('#app'),
@@ -30,6 +29,19 @@ $(() => {
     //调用方法
     app.setScreen()
     console.log(app.getUrlPrama('table_id'))
+
+    var typeSelect = new MobileSelect({
+        trigger: '.uploader',
+        title: '选择相册',
+        wheels: [
+            {data: [{id: '1', value: '家庭住房状况-之前', mark: 1, type: 3}, {id: '2', value: '家庭住房状况-现在', mark: 2, type: 3}, {id: '3', value: '家庭饮水状况-之前', mark: 1, type: 4}, {id: '4', value: '家庭饮水状况-现在', mark: 2, type: 4}]}
+        ],
+        callback: function (indexArr, data) {
+            console.log(data)
+            $('.uploader').html('')
+            window.location = `uploader.html?familyid=${app.getUrlPrama('table_id')}&type=${data[0].type}&mark=${data[0].mark}&filingyear=${$('.uploader').attr('data-year')}`
+        }
+    })
 
     //生成列表链接
     $('#basic').attr('href', `poordetail_basic.html?table_id=${app.getUrlPrama('table_id')}`)
@@ -72,6 +84,7 @@ $(() => {
         type: 'POST',
         data: `table_id=${app.getUrlPrama('table_id')}`,
         success: (data) => {
+            $('.uploader').attr('data-year', JSON.parse(data).data.poor.filingyear)
             console.log(JSON.parse(data).data)
             $('#name').html(JSON.parse(data).data.poor.name)
             $('#area').html(JSON.parse(data).data.poor.townname + '&nbsp;' + JSON.parse(data).data.poor.villagename)
