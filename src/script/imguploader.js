b63da5c10b3aa827e36ce518a0506daffa16d45e
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-08-15 18:02:32 +0800

    new

--- src/script/imguploader.js
+++ src/script/imguploader.js
@@ -14,29 +14,88 @@
  */
 var popupBtn = () => {
     
-    $('body').append(`<div class="popupBtn"></div>`)
+    $('body').append(`<div class="iu-mask"></div><div class="popupBtn"><div id="iu-camera" class="unit btn">拍照</div><div id="iu-photo" class="unit btn">相册</div><div id="iu-cancle" class="unit cancle">取消</div></div>`)
 
 }
 
-var showBtn = (btn) => {
-    $('.ui-mask').show()
+var showBtn = () => {
+
+    if ($('.popupBtn').hasClass('animOut')) {
+
+        $('.popupBtn').removeClass('animOut')
+
+    }
+
+    $('.popupBtn').addClass('animIn')
+    $('.iu-mask').show()
 }
 
-var hideBtn = (btn) => {
-    
-    $('.ui-mask').hide()
+var hideBtn = () => {
+
+    if ($('.popupBtn').hasClass('animIn')) {
+        
+        $('.popupBtn').removeClass('animIn')
+        
+    }
+    $('.popupBtn').addClass('animOut')
+    $('.iu-mask').hide()
+}
+
+var getpicture = (source) => {
+    if (source) {
+        navigator.camera.getPicture(
+            onPhotoSuccess, 
+            function (error) {
+                console.log("照片获取失败！")
+            }, 
+            { 
+                destinationType: Camera.DestinationType.FILE_URI, 
+                sourceType: source 
+            }
+        )
+    } else {
+        navigator.camera.getPicture(
+            onPhotoSuccess, 
+            function (error) {
+                console.log("照片获取失败！")
+            }, 
+            { 
+                destinationType: Camera.DestinationType.FILE_URI
+            }
+        )
+    }
+}
+
+var onPhotoSuccess = (imgData) => {
+
 }
 
 var onDeviceReady = () => {
     popupBtn()
 
-    $()
+    $(document).on('click', '#iu-camera', function () {
+        getpicture()
+    })
+
+    $(document).on('click', '#iu-photo', function () {
+        getpicture()
+    })
 }
 
 $(() => { 
+    popupBtn()
+
     document.addEventListener("deviceready", onDeviceReady, false)
 
     $(document).on('click', '#iu-cancle', () => {
-        hideBtn('#iu-cancle')
+        hideBtn()
+    })
+
+    $(document).on('click', '.iu-mask', () => {
+        hideBtn()
+    })
+
+    $('#btn').click(function () {
+        showBtn()
     })
 })
